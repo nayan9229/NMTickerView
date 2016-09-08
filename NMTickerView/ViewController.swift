05f014e9378a97f502eac95eae0a363d57ac7dbd
nayan <nayan@nayans-macbook-pro.local>
2016-09-08 13:30:04 +0530

    Initial Commit

---
+++ NMTickerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NMTickerView
+//
+//  Created by nayan on 08/09/16.
+//  Copyright © 2016 vishavatech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
